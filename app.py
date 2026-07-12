37a090b518a59b46f6f35144fe231069908d5b45
juss <86586334+Jussd@users.noreply.github.com>
2026-07-12 16:25:17 +0300

    Create app.py as entry point for application
    Add entry point for PyInstaller to run main function.

---
+++ app.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Entry point for PyInstaller. Keeps main code in lazy_comments.py."""
+
+from lazy_comments.lazy_comments import main
+
+if __name__ == "__main__":
+    main()

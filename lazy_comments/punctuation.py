117bcf54d2c477af92526114aa55e3d5c25d3420
Jussd <silicon.odessa@gmail.com>
2026-07-14 15:01:40 +0300

    Fix ImportError: rename terms.py -> term_replacements.py to avoid name clash with terms/ package

--- lazy_comments/punctuation.py
+++ lazy_comments/punctuation.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 import re
 
-from lazy_comments.terms import TERM_REPLACEMENTS
+from lazy_comments.term_replacements import TERM_REPLACEMENTS
 
 
 QUESTION_WORDS = {
